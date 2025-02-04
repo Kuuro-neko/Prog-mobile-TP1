db73e7762d6b15b5e62210630767405fa98bc055
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-04 16:36:36 +0100

    Exo 7 done

--- app/src/main/java/com/example/prog_mobile_tp1/PhoneActivity.java
+++ app/src/main/java/com/example/prog_mobile_tp1/PhoneActivity.java
@@ -1,6 +1,14 @@
 package com.example.prog_mobile_tp1;
 
+import static android.content.Intent.getIntent;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +23,20 @@ public class PhoneActivity extends MenuActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_phone);
+
+        TextView phoneTextView = findViewById(R.id.label_phone);
+        phoneTextView.setText(getIntent().getStringExtra("phone"));
+
+        Uri phoneUri = getIntent().getStringExtra("phone") != null ? Uri.parse("tel:" + getIntent().getStringExtra("phone")) : null;
+
+        Button callButton = findViewById(R.id.button_call);
+        callButton.setOnClickListener(v -> {
+            if (phoneUri != null) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL, phoneUri);
+                startActivity(callIntent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
